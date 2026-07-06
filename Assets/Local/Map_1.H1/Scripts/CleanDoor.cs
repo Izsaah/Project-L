8fcd049622b51f2171232db95dc24ae0d6c0ed8f
ThinhLe1108 <leducthinh.110805@gmail.com>
2026-07-06 17:31:19 +0700

    blablabla

--- Assets/Local/Map_1.H1/Scripts/CleanDoor.cs
+++ Assets/Local/Map_1.H1/Scripts/CleanDoor.cs
@@ -4,63 +4,73 @@ using UnityEngine;
 using DG.Tweening;
 using ProjectL.Scripts.Interface;
 
-public class CleanDoor : MonoBehaviour
+namespace ProjectL.Local.Map_1_H1
 {
-    public float openAngle = 90f;
-    public float swingSpeed = 1.5f;
+        public class CleanDoor : MonoBehaviour
+    {
+        public float openAngle = 90f;
+        public float swingSpeed = 1.5f;
 
-    private bool isOpen = false;
-    private bool isPlayerNear = false; 
-    private Vector3 closedRotation;
+        private bool isOpen = false;
+        private bool isPlayerNear = false; 
+        public bool isLocked = true;
+        private Vector3 closedRotation;
 
-    private IInputProvider inputProvider;
-    
-    void Start(){
-        closedRotation = transform.localEulerAngles;
-        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        private IInputProvider inputProvider;
         
-        if (playerObj != null)
-        {
-            inputProvider = playerObj.GetComponent<IInputProvider>();
+        void Start(){
+            closedRotation = transform.localEulerAngles;
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            
+            if (playerObj != null)
+            {
+                inputProvider = playerObj.GetComponent<IInputProvider>();
+            }
+            else
+            {
+                Debug.LogError("CleanDoor could not find the Player! Make sure your player character has the 'Player' tag.");
+            }
         }
-        else
+
+        void Update()
         {
-            Debug.LogError("CleanDoor could not find the Player! Make sure your player character has the 'Player' tag.");
+            if (!isLocked && isPlayerNear && inputProvider != null && inputProvider.GetInteract())
+            {
+                ToggleDoor();
+            }
         }
-    }
 
-    void Update()
-    {
-        if (isPlayerNear && inputProvider != null && inputProvider.GetInteract())
+        void ToggleDoor()
         {
-            ToggleDoor();
+            isOpen = !isOpen;
+            
+            // 2. THIS IS THE FIX! We calculate the new angle based on the snapshot we took in Start()
+            Vector3 targetRotation = isOpen ? 
+                new Vector3(closedRotation.x, closedRotation.y, closedRotation.z + openAngle) : 
+                closedRotation;
+
+            // 3. We apply that calculated rotation
+            transform.DOLocalRotate(targetRotation, swingSpeed).SetEase(Ease.InOutQuad);
         }
-    }
 
-    void ToggleDoor()
-    {
-        isOpen = !isOpen;
-        
-        // 2. THIS IS THE FIX! We calculate the new angle based on the snapshot we took in Start()
-        Vector3 targetRotation = isOpen ? 
-            new Vector3(closedRotation.x, closedRotation.y, closedRotation.z + openAngle) : 
-            closedRotation;
-
-        // 3. We apply that calculated rotation
-        transform.DOLocalRotate(targetRotation, swingSpeed).SetEase(Ease.InOutQuad);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) 
+        public void UnlockDoor()
         {
-            isPlayerNear = true;
-            Debug.Log(gameObject.name + " was triggered by " + other.gameObject.name);
+            isLocked = false;
+            Debug.Log("The door has been unlocked!");
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player")) isPlayerNear = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) 
+            {
+                isPlayerNear = true;
+                Debug.Log(gameObject.name + " was triggered by " + other.gameObject.name);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player")) isPlayerNear = false;
+        }
     }
 }
