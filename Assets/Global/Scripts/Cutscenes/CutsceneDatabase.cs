a1d4cd5c1272f59c09a3f7ba989b18fc12ae3796
Nguyễn Võ Hoàng Long <longxautrai12@gmail.com>
2026-07-14 22:15:39 +0700

    dlisclose;-;

--- Assets/Global/Scripts/Cutscenes/CutsceneDatabase.cs
+++ Assets/Global/Scripts/Cutscenes/CutsceneDatabase.cs
@@ -1,34 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 namespace ProjectL.Global.Script.CutScenes
 {
-    [CreateAssetMenu(fileName = "CutSceneDB", menuName = "ProjectL/CutSceneDB")]
+    // You keep the same script name, but now it builds individual cutscenes!
+    [CreateAssetMenu(fileName = "New Cutscene", menuName = "ProjectL/CutScene")]
     public class CutsceneDatabase : ScriptableObject
     {
-        [System.Serializable]
-        public struct CutsceneLine
-        {
-            [Tooltip("e.g., 'granny_saved'")]
-            public string key;
-
-            [Tooltip("Drag the actual 2d picture")]
-            public Sprite[] imageSprite;
-        }
-        public List<CutsceneLine> db = new List<CutsceneLine>();
-        public Sprite[] getImage(string key)
-        {
-            foreach (CutsceneLine line in db)
-            {
-                if (line.key == key)
-                {
-                    return line.imageSprite;
-                }
-            }
-            Debug.LogWarning("carefull");
-            return null;
-        }
-
+        [Tooltip("Drag the actual 2D pictures for THIS specific cutscene here")]
+        public Sprite[] images;
     }
 }
-
